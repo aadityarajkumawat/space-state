62e7227c5f444248f798f689abd4fa3a7ac8e529
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-06-02 16:50:39 +0530

    added a desc for createState

--- src/index.js
+++ src/index.js
@@ -9,6 +9,21 @@ function initState(key, initialState) {
     return state;
 }
 
+/**
+ * This is used to create state instance it takes an initialState
+ * as its only parameter and instantly pushes that state to store.
+ *
+ * It exposes two API methods which are called getState and setState,
+ * which are used to retrive and update state value.
+ *
+ * The setState function takes a parameter which can either be a state
+ * value or it can be a function which has the to be previous state as
+ * it parameter and leverage the access of that state value. This can
+ * be helpful in cases when we want to push new items to an array state.
+ *
+ * @param {object} initialState
+ * @returns ````getState, setState````
+ */
 export function createState(initialState) {
     const key = uid();
     initState(key, initialState);
